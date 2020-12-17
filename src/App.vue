fbaa3caa111782264fe54fdd5c846cbf4ac94eed
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2020-12-17 21:35:13 +0600

    project init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <github-card v-model="username"></github-card>
+  </div>
+</template>
+
+<script>
+import GithubCard from "./components/GithubCard";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      username: "",
+    };
+  },
+  components: {
+    GithubCard,
+  },
+};
+</script>
